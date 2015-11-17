7c7934b579707beba8122eeb7cea8c32195ce5c2
caochitam002 <caochitam002@gmail.com>
2015-11-17 09:00:16 +0200

    x

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var app = angular.module('vhh', ['ui.router']);
+var templates = {};
+templates.login = '';
+app.config(function($stateProvider, $urlRouterProvider) { //Router config
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+    .state('home', {
+        url: '/home',
+        templateUrl: 'template/home.html'
+    })
+    .state('login', {
+        url: '/login',
+        template: 'template/login.html'
+    });
+});
+        //app.controller('loginCtrl',function($scope){});
+        //app.controller('indexCtrl',function($scope){});
